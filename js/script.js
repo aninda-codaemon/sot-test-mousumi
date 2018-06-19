1820862a896f65bebecef4174273c661c90a8216
Aninda Kar <aninda.kar@outlook.com>
2018-06-19 10:21:11 +0530

    style changes

--- js/script.js
+++ js/script.js
@@ -5,33 +5,28 @@ $(window).on('load', function () {
     var $ajaxLoader = $('.ajax-load');
     var msgSteps = 0;
     var duration = 30000;
-    var stepDuration = duration / $progressBarMessage.length + 2;
-
-    /* Preloader */
-    $('#preloader').delay(550).fadeOut('slow');
-
-    /* Scroll Top */
-    $('.jumper').on('click', function(e) {
-      e.preventDefault();
-      $('body, html').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-      }, 1000);
-    });
+    var stepDuration = (duration / $progressBarMessage.length) + 2;
+    var searchData = {};
 
     function initProgressBar() {
-      var initialProgress = $($progressBar).animate({ width: '100%' }, {
+      msgSteps = 0;
+      $($progressBar).css('width', '0%');
+      $('#show-data').hide();
+      $('.table tbody').html('');
+      $('#progress-bar-container').show();
+      var barProgress = $($progressBar).animate({ width: '100%' }, {
         duration,
         progress(animation, progress) {
           var progression = Math.ceil(progress * 100);
           $($progressBar).html(progression + '%');
-          console.log(progression + " " + progress);
+          // console.log(progression + " " + progress);
         },
       });
       showStepMessages();
-      $.when(initialProgress).done(() => {
+      $.when(barProgress).done(() => {
         window.setTimeout(function() {
           $('#progress-bar-container').hide();
-          $('#show-data').removeClass('hidden');
+          $('#show-data').show();
         }, 3000);
       });
     }
@@ -47,25 +42,27 @@ $(window).on('load', function () {
       }
     }
 
-    $(window).scroll(function() {
-      // if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-      // if ($('#show-data').height() >= $('.table-responsive').height()) {
+    function pageScroller() {
+      $(window).scroll(function() {
+        // if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+        // if ($('#show-data').height() >= $('.table-responsive').height()) {
 
-      if ($('#show-data').is(':visible')) {
-        shouldLoadData = true;
-        if (shouldLoadData) {
-          if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-            shouldLoadData = false;
-            console.log('Scrolling: ' + ($(window).scrollTop() + $(window).height()) + ' ' + $(document).height());
-            console.log('shouldLoadData outside: ' + shouldLoadData);
-            console.log('shouldLoadData inside: ' + shouldLoadData);
-            // var last_id = $(".post-id:last").attr("id");
-            // loadMoreData(last_id);
-            showPageLoader();
+        if ($('#show-data').is(':visible')) {
+          shouldLoadData = true;
+          if (shouldLoadData) {
+            if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+              shouldLoadData = false;
+              console.log('Scrolling: ' + ($(window).scrollTop() + $(window).height()) + ' ' + $(document).height());
+              console.log('shouldLoadData outside: ' + shouldLoadData);
+              console.log('shouldLoadData inside: ' + shouldLoadData);
+              // var last_id = $(".post-id:last").attr("id");
+              // loadMoreData(last_id);
+              showPageLoader();
+            }
           }
         }
-      }
-    });
+      });
+    }
 
     function showPageLoader() {
       $ajaxLoader.show();
@@ -76,38 +73,111 @@ $(window).on('load', function () {
       }, 5000);
     }
 
-    function loadMoreData() {
+    function getSearchUrl(searchedData) {
+      var searchQuery = '';
+      var baseUrl = "https://data.cityofnewyork.us/resource/aiza-48ch.json?$limit=100&$offset=0";
+      var appID = (searchedData.appId ? searchedData.appId : '');
+      var businessName = searchedData.businessName ? encodeURI(searchedData.businessName) : '';
+      var cityName = searchedData.cityName ? encodeURI(searchedData.cityName) : '';
+
+      if (appID) {
+        searchQuery += '&application_id=' + appID.toString();
+      }
+
+      if (businessName) {
+        searchQuery += '&business_name=' + businessName;
+      }
+
+      if (cityName) {
+        searchQuery += '&city=' + cityName;
+      }
+
+      var url = baseUrl + searchQuery;
+      return url;
+    };
+
+    function getSearchedData(searchedData) {
+      var xhrData = $.ajax({
+        url: getSearchUrl(searchedData),
+        dataType: 'json',
+        // jsonpCallback: 'parseResults',
+        statusCode: {
+          503: function () {
+            // trackNL("Search Throttled");
+            userThrottled = true;
+          }
+        }
+      });
+      excessResults = false;
+      return $.when(xhrData)
+        .then(function(xhrResult) {
+          console.log('Ajax Data: ' + xhrResult);
+          $.each(xhrResult, function(key, row) {
+            console.log('Ajax Each: ' + row.application_id);
+            createRowData(row);
+          });
+          // After the fetching and calculation is done
+          // Start the progress bar
+          // startTheProgressBar();
+          return teaserDataObj;
+        });
+    }
+
+    function createRowData(row) {
       console.log('loadMoreData');
       var appendData = '<tr>'
-                          + '<td>1</td>'
-                          + '<td>Mark</td>'
-                          + '<td>Otto</td>'
-                          + '<td>@mdo</td>'
-                          + '<td>a@mdo.com</td>'
-                          + '<td></td>'
-                        + '</tr>'
-                        + '<tr>'
-                          + '<td>2</td>'
-                          + '<td>Jacob</td>'
-                          + '<td>Thornton</td>'
-                          + '<td>@fat</td>'
-                          + '<td>b@fat.com</td>'
-                          + '<td></td>'
-                        + '</tr>'
-                        + '<tr>'
-                          + '<td>3</td>'
-                          + '<td colspan="2">Larry the Bird</td>'
-                          + '<td>@twitter</td>'
-                          + '<td>c@twitter.com</td>'
-                          + '<td></td>'
+                          + '<td class="hidden-xs">'+row.application_id+'</td>'
+                          + '<td class="hidden-xs">'+row.application_category+'</td>'
+                          + '<td class="hidden-xs">'+row.business_name+'</td>'
+                          + '<td class="hidden-xs">'+row.application_or_renewal+'</td>'
+                          + '<td class="hidden-xs">'+(row.license_number?row.license_number:'N/A')+'</td>'
+                          + '<td class="hidden-xs">'+row.city+'</td>'
+                          + '<td class="hidden-xs">'+row.status+'</td>'
+                              + '<td class="visible-xs table-mob col-xs-12">'
+                                + '<h2>'+row.application_id+'</h2>'
+                                + '<h4><i class="fa fa-user-o" aria-hidden="true"></i>'+row.application_category+'</h4>'
+                                + '<p><i class="fa fa-phone" aria-hidden="true"></i>'+row.business_name+'</p>'
+                                + '<p><i class="fa fa-envelope-o" aria-hidden="true"></i>'+row.application_or_renewal+'</p>'
+                                + '<p><i class="fa fa-envelope-o" aria-hidden="true"></i>'+(row.license_number?row.license_number:'N/A')+'</p>'
+                                + '<p><i class="fa fa-map-marker" aria-hidden="true"></i>'+row.city+'</p>'
+                                + '<p><i class="fa fa-address-card-o" aria-hidden="true"></i>'+row.status+'</p>'
+                              + '</td>'
                         + '</tr>';
       $('.table tbody').append(appendData);
     }
 
-    $('#filter-search').on('click', function(e) {
+    function filterSearchCallback(e) {
       e.preventDefault();
-      initProgressBar();      
-    });
+      searchData.appId = $('#txt_app_id').val().trim();
+      searchData.businessName = $('#txt_business_name').val().toLowerCase().trim();
+      searchData.cityName = $('#txt_city_name').val().trim();
+      // var cityHasLetters = /[a-z]+/i.test(searchData.cityName);
+      searchData.response = getSearchedData(searchData);
+
+      initProgressBar();
+      pageScroller();
+    }
+
+    function addEvents() {
+      $('#filter-search').on('click', filterSearchCallback);
+
+      /* Preloader */
+      $('#preloader').delay(550).fadeOut('slow');
+
+      /* Scroll Top */
+      $('.jumper').on('click', function(e) {
+        e.preventDefault();
+        $('body, html').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+      });
+    }
+
+    function init() {
+      addEvents();
+    }
+
+    init();
 });
 
 /* Team */
